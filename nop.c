5f7c311faad31518ae9b41c36cef8ef731391c8f
seleshi27 <110991479+seleshi27@users.noreply.github.com>
2023-03-24 01:05:00 +0300

    Create nop.c

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+ * m_nop - doesn't do anything
+ * @head: pointer to head
+ * @count_lines: current line number
+ * Return: void function
+ */
+void m_nop(stack_t **head, unsigned int count_lines)
+{
+	(void) head;
+	(void) count_lines;
+}
