c579161d532ce246604eb77e75d155d7e69f4acf
seleshi27 <110991479+seleshi27@users.noreply.github.com>
2023-03-24 02:12:48 +0300

    Update pall.c

--- pall.c
+++ pall.c
@@ -1,20 +1,26 @@
 #include "monty.h"
-/**
- * m_pall - print the stack
- * @head: doble pointer to head of d linked list
- * @line_count: current line of monty file
- * Return: returns void
- */
-void m_pall(stack_t **head, __attribute__((unused)) unsigned int line_count)
-{
-	stack_t *temp = *head;
 
-	if (*head && head)
+/**
+ * pall - function that prints all elements on the stack
+ * @stack: pointer to head of the stack
+ * @line_number: where the instruction appears
+ * Description: 0. push, pall
+ * Return: see below
+ * 1. upon success, nothing
+ * 2. upon fail, EXIT_FAILURE
+ */
+void get_pall(stack_t **stack, unsigned int line_number)
+{
+	stack_t *current;
+
+	(void)line_number;
+
+	current = *stack;
+
+	while (current != NULL)
 	{
-		while (temp)
-		{
-			printf("%d\n", temp->n);
-			temp = temp->next;
-		}
+		printf("%d", current->n);
+		current = current->next;
+		printf("\n");
 	}
 }
